7b5c55b21932929c08adaacde3e32637468ff13e
manjunath h k <manjunathhk833@gmail.com>
2021-11-17 14:49:11 +0530

    integrated firebase and set up authentication using google sign in method as well as completed sign in page styling for email sign in

---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,23 @@
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/firestore';
+import  'firebase/compat/auth';
+
+const config = {
+    apiKey: "AIzaSyCSVzRkrlKzFYx876QHnPN_iKdva03COLY",
+    authDomain: "crwn-clothing-8796c.firebaseapp.com",
+    projectId: "crwn-clothing-8796c",
+    storageBucket: "crwn-clothing-8796c.appspot.com",
+    messagingSenderId: "807801847371",
+    appId: "1:807801847371:web:59fc8545e540e9730417ce"
+  };
+
+  firebase.initializeApp(config);
+
+  export const auth = firebase.auth();
+  export const firestore = firebase.firestore();
+
+  const provider = new firebase.auth.GoogleAuthProvider();
+  provider.setCustomParameters({prompt: 'select_account' });
+  export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+  export default firebase;
