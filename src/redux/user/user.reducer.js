eeaa2855bfb42adb4f9d656ec42695b2bc4f9b60
manjunath h k <manjunathhk833@gmail.com>
2021-11-19 14:44:49 +0530

    added sign in redirect but there's a sign in bug that needs fix

--- src/redux/user/user.reducer.js
+++ src/redux/user/user.reducer.js
@@ -1,10 +1,12 @@
+import { UserActionTypes } from "./user.types";
+
 const INITIAL_STATE = {
     current_user : null
 }
 
 const userReducer = (state = INITIAL_STATE, action) => {
     switch(action.type){
-        case 'SET_CURRENT_USER':
+        case UserActionTypes.SET_CURRENT_USER:
             return {
                 ...state,
                 current_user: action.payload
