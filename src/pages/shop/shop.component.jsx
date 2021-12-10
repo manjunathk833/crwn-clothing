e55a261e38b0679d81b4703f016aacd9567a485b
manjunath h k <manjunathhk833@gmail.com>
2021-12-10 19:49:35 +0530

    used promise to fetch firestore shop data

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -28,7 +28,7 @@ class ShopPage extends React.Component{
     componentDidMount() {
         const {updateCollections} = this.props;
         const collectionRef = firestore.collection('collections');
-        collectionRef.onSnapshot(async snapshot => {
+        collectionRef.get().then(async snapshot => {
             const collectionsMap = convertCollectionSnapshotToMap(snapshot);
             updateCollections(collectionsMap);
             this.setState({loading: false});
